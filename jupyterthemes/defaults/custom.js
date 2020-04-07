82b4428c79f9d556c260484be6e3cdd82e4bb18b
Osama Sakhi <msakhi21@gmail.com>
2020-04-07 18:14:43 +0000

    Sublime keybindings

--- jupyterthemes/defaults/custom.js
+++ jupyterthemes/defaults/custom.js
@@ -80,3 +80,17 @@
  * @class customjs
  * @static
  */
+
+
+require(["codemirror/keymap/sublime", "notebook/js/cell", "base/js/namespace"],
+  function(sublime_keymap, cell, IPython) {
+    // setTimeout(function(){ // uncomment line to fake race-condition
+    cell.Cell.options_default.cm_config.keyMap = 'sublime';
+    var cells = IPython.notebook.get_cells();
+    for(var cl=0; cl< cells.length ; cl++){
+      cells[cl].code_mirror.setOption('keyMap', 'sublime');
+    }
+
+    // }, 1000)// uncomment  line to fake race condition
+  }
+);
